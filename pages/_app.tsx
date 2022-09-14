ca69dd58cfff0d3b61979dfbc928e7d2e75107f5
CPlusPatch <contact@cpluspatch.com>
2022-09-14 13:48:05 -1000

    fix compile bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,8 @@ import '../styles/globals.css';
 import Head from 'next/head';
 import { SessionProvider } from "next-auth/react";
 import NextNProgress from "nextjs-progressbar";
-import type { AppProps } from 'next/app';
 
-function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function App({ Component, pageProps: { session, ...pageProps } }) {
 	return (
 		<SessionProvider session={session}>
 			<Head>
